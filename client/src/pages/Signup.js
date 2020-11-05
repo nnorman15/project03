39d5326ea07949355421dd9c8ee10aefa5b268ea
nnorman15 <nnorman15@gmail.com>
2020-11-05 13:42:31 -0700

    fixing signup title color

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -39,7 +39,7 @@ const Signup = () => {
     <main className='flex-row justify-center mb-4'>
       <div className='col-12 col-md-6'>
         <div className='card'>
-          <h4 className='card-header'>Sign Up</h4>
+          <h4 className='card-headerTitle'>Sign Up</h4>
           <div className='card-body'>
             <form onSubmit={handleFormSubmit}>
               <input
